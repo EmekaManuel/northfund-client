ef03430602bbc70942335d36cc490a75be3d76e3
Onyewuchi Emeka <marrnuel123@.com>
2024-09-29 11:16:35 +0100

    chore(): added the footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <div className="z-20 w-full bg-background/95 shadow backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="mx-4 flex h-14 items-center md:mx-8">
+        <p className="text-left text-xs leading-loose text-muted-foreground md:text-sm">
+          Built by{" "}
+          <Link
+            to="https://github.com/Kds-JS"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="font-medium underline underline-offset-4"
+          >
+            Kds
+          </Link>
+          . The source code is available on{" "}
+          <Link
+            to="https://github.com/Kds-JS/crowdfunding-solana"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="font-medium underline underline-offset-4"
+          >
+            GitHub
+          </Link>
+          .
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
