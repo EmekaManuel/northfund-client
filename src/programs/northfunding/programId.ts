27cf23a65711dcdb2ec266570ff88c38a9e38fdb
Onyewuchi Emeka <marrnuel123@.com>
2024-10-02 10:33:55 +0100

    chore(): refactor the getprogramid func

--- src/programs/northfunding/programId.ts
+++ src/programs/northfunding/programId.ts
@@ -4,9 +4,18 @@ import { PublicKey } from "@solana/web3.js";
 const MAINNET_PROGRAM_ID = import.meta.env.VITE_MAINNET_PROGRAM_ID as string;
 const DEVNET_PROGRAM_ID = import.meta.env.VITE_DEVNET_PROGRAM_ID as string;
 
-export const getProgramId = (network: NetworkName) => {
-  const programId = new PublicKey(
-    network === NetworkName.Mainnet ? MAINNET_PROGRAM_ID : DEVNET_PROGRAM_ID
-  );
-  return programId;
+export const getProgramId = (network: NetworkName): PublicKey => {
+  const programIdString =
+    network === NetworkName.Mainnet ? MAINNET_PROGRAM_ID : DEVNET_PROGRAM_ID;
+
+  if (!programIdString) {
+    throw new Error(`Program ID for ${network} is not defined`);
+  }
+
+  try {
+    return new PublicKey(programIdString);
+  } catch (err) {
+    console.log(err);
+    throw new Error(`Invalid Program ID for ${network}: ${programIdString}`);
+  }
 };
